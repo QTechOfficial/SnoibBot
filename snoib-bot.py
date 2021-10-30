4132e87e93fa1bd97fda93827cd9ec84468bae23
Quantum <thomasmuller336@gmail.com>
2021-10-30 16:56:21 -0400

    Some cleanup
    Moved everything into SnoibBot class that extends discord.Client
    
    Removed unused imports
    
    Moved CHANNELID and TOKEN constants to main() to make it easier to move
    them to a file later
    
    Made member variables private
    
    Added boilerplate if __name__ == '__main__'

--- snoib-bot.py
+++ snoib-bot.py
@@ -1,70 +1,82 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import time
-import random
-import logging
-import discord
-import asyncio
-from discord.ext import tasks
+from discord import Client
 from re import findall
-from urllib.request import urlopen
-
-TOKEN = ''
-CHANNELID = 0
-
-#######################
+import asyncio
+import logging
+import random
 
 
-logger = logging.getLogger('SnoibBot')
-logger.setLevel(logging.DEBUG)
-f = logging.Formatter('[%(levelname)s]: %(message)s')
-sh = logging.StreamHandler()
-sh.setLevel(logging.DEBUG)
-sh.setFormatter(f)
-fh = logging.FileHandler('snoibbot.log')
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(f)
-logger.addHandler(sh)
-logger.addHandler(fh)
+class SnoibBot(Client):
+    def __init__(self, channel_id):
+        super().__init__()
 
-client = discord.Client()
+        self._channel_id = channel_id
 
-def contains(msg, list):
-    return sum([len(findall(word, msg.lower())) for word in list])
-    # return any([fnmatch(msg, '*'+item+'*') for item in list])
+        self._logger = logging.getLogger('SnoibBot')
+        self._logger.setLevel(logging.DEBUG)
 
-def classify(message):
-    msg = message.content
-    print(msg)
+        f = logging.Formatter('[%(levelname)s]: %(message)s')
 
-    response = "test"
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.DEBUG)
+        sh.setFormatter(f)
 
-    return response
+        fh = logging.FileHandler('snoibbot.log')
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(f)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+        self._logger.addHandler(sh)
+        self._logger.addHandler(fh)
 
-    response = None
+    def count_words(self, msg, list_):
+        '''
+        Returns the number of times all words from the list list_ shows up in the message msg
+        '''
+        return sum([len(findall(word, msg.lower())) for word in list_])
+        # return any([fnmatch(msg, '*'+item+'*') for item in list_])
 
-    if f"<@!{client.user.id}" in message.content:
-        logger.info('User has dared to mention the all-knowing Snoib bot! Smite them!')
-        response = mentioned[random.randint(0, len(mentioned) - 1)]
+    def classify(self, message):
+        msg = message.content
+        print(msg)
 
-    elif message.channel.id == CHANNELID:
-        logger.info('Received message from {0}: {1}, classifying it.'.format(message.author.name, message.content))
-        response = classify(message)
+        response = "test"
 
-    if response:
-        try:
-            await message.reply(response, mention_author=True)
-        except:
-            await client.get_channel(CHANNELID).send(message.author.mention+' '+response)
+        return response
 
-@client.event
-async def on_ready():
-    logger.info('Logged in as {0}({1})!'.format(client.user.name, client.user.id))
+    async def on_message(self, message):
+        print(message)
+        if message.author == self.user:
+            return
 
-client.run(TOKEN)
+        response = None
+
+        if f"<@!{self.user.id}" in message.content:
+            self._logger.info('User has dared to mention the all-knowing Snoib bot! Smite them!')
+            response = mentioned[random.randint(0, len(mentioned) - 1)]
+
+        elif message.channel.id == self._channel_id:
+            self._logger.info('Received message from {0}: {1}, classifying it.'.format(message.author.name, message.content))
+            response = classify(message)
+
+        if response:
+            try:
+                await message.reply(response, mention_author=True)
+            except:
+                await self.get_channel(self._channel_id).send(message.author.mention+' '+response)
+
+    async def on_ready(self):
+        self._logger.info(f'Logged in as {self.user.name}({self.user.id})!')
+
+
+def main():
+    TOKEN = ''
+    CHANNEL_ID = 0
+
+    bot = SnoibBot(CHANNEL_ID)
+    bot.run(TOKEN)
+
+
+if __name__ == '__main__':
+    main()
